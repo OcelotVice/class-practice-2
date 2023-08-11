7de155db36fd4c6c88de2915adc2026a839e0ff8
gabriel <iamgabrielvice@gmail.com>
2023-08-11 14:18:09 +1000

    first-commit

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,24 @@
 // Your code here
+const isEsModule = require('sinon/lib/sinon/util/core/is-es-module');
+const Dragon = require('../classes/dragon');
+const exportAsyncBehaviors = require('sinon/lib/sinon/util/core/export-async-behaviors');
+
+class EvilDragon extends Dragon {
+    constructor (name,color,evilDoings, nemesis) {
+        super(name,color);
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+
+    dontInviteThemOverForDinner() {
+        this.evilDoings.forEach(deed => {
+            console.log(`${this.name} will ${deed}`)
+        })
+    }
+
+    burnsNemesis() {
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`;
+    }
+}
+
+module.exports = EvilDragon;
--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,30 @@
 // Your code here
+const isEsModule = require('sinon/lib/sinon/util/core/is-es-module');
+const Dragon = require('../classes/dragon');
+
+
+class FriendlyDragon extends Dragon {
+    constructor(name,color,lifeGoals,friend) {
+        super(name,color);
+        this.lifeGoals = lifeGoals;
+        this.friend = friend;
+    }
+
+    hasLifeGoals() {
+        for(let i = 0; i < this.lifeGoals.length; i++) {
+            let goal = this.lifeGoals[i];
+            console.log(`${this.name} likes to ${goal}`);
+        }
+    }
+
+    helpsPeople() {
+        return `${this.name} helps their friend ${this.friend}`;
+    }
+}
+
+
+try {
+    module.exports = FriendlyDragon;
+} catch {
+    module.exports = null;
+}
